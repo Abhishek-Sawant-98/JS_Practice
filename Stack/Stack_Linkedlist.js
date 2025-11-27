f5dc8b64dc51597ad734a5187b895388766f016d
Abhishek Sawant <abhishakesawant@gmail.com>
2025-11-27 13:34:05 +0530

    fix: use minimalist implementation

--- Stack/Stack_Linkedlist.js
+++ Stack/Stack_Linkedlist.js
@@ -3,93 +3,38 @@ class Node {
     constructor(data){
         this.data = data;
         this.next = null;
-        this.prev = null;
-    }
-}
-class CircularDoublyLinkedlist {
-    constructor(head) {
-        this.head = head ?? null;
-    }
-    push(data){
-        const newNode = new Node(data);
-
-        if ( this.head === null ) {
-            this.head = newNode;
-            this.head.next = this.head;
-            this.head.prev = this.head;
-            return;
-        }
-
-        newNode.next = this.head;
-        newNode.prev = this.head.prev;
-        this.head.prev.next = newNode;
-        this.head.prev = newNode;
-    }
-    pop(){
-        if ( this.head === null ) return null;
-
-        if ( this.head.prev === this.head ) {
-            const headData = this.head.data;
-            this.head = null;
-            return headData;
-        }
-
-        const removed = this.head.prev.data;
-        this.head.prev = this.head.prev.prev;
-        this.head.prev.next = this.head;
-        return removed;
-    }
-    peek(){
-        if ( this.head === null ) return null;
-
-        return this.head.prev.data;
-    }
-    print(){
-        let current = this.head,
-            output = '';
-
-        if ( this.head === null ) {
-            console.log(null);
-            return;
-        }
-
-        do {
-            output += `${current.data} <-> `;
-            current = current.next;
-        } while ( current !== this.head );
-
-        output += 'back to head';
-
-        console.log(output);
     }
 }
 class Stack {
     constructor(){
-        this.#list = new CircularDoublyLinkedlist();
+        this.head = null;
     }
 
-    #list = null;
     #count = 0;
 
     isEmpty(){
-        return this.#count === 0;
+        return this.head === null;
     }
     push(data){
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
         this.#count++;
-        this.#list.push(data);
     }
     pop(){
+        if ( this.head === null ) return null;
+
+        const data = this.head.data;
+        this.head = this.head.next;
         this.#count--;
-        return this.#list.pop();
+        return data;
     }
     peek(){
-        return this.#list.peek();
+        if ( this.head === null ) return null;
+        return this.head.data;
     }
     size(){
         return this.#count;
-    }
-    print(){
-        this.#list.print();
     }
 }
 // Driver code
@@ -97,9 +42,9 @@ const stack = new Stack();
 console.log(stack.isEmpty());
 stack.push(10);
 stack.push(20);
-stack.print()
 console.log(stack.size());
 console.log(stack.peek());
 console.log(stack.isEmpty());
 stack.pop();
-stack.print();
+console.log(stack.size());
+console.log(stack.peek());
